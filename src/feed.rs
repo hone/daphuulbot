eb067b058cc6e1035b1be5a24d1dc6f42e7bdd83
Terence Lee <hone02@gmail.com>
2020-11-06 15:58:40 -0600

    refactor feed hook into its own module

---
+++ src/feed.rs
@@ -0,0 +1,96 @@
+use lazy_static::lazy_static;
+use linkify::{Link, LinkFinder, LinkKind};
+use regex::Regex;
+use serenity::{
+    client::Context,
+    framework::standard::macros::hook,
+    model::prelude::{ChannelId, Message},
+    prelude::TypeMapKey,
+};
+use std::collections::HashSet;
+
+pub struct PostableChannels;
+impl TypeMapKey for PostableChannels {
+    type Value = HashSet<ChannelId>;
+}
+
+#[hook]
+pub async fn normal_message_hook(ctx: &Context, msg: &Message) {
+    let data = ctx.data.read().await;
+    let postable_channels = data
+        .get::<PostableChannels>()
+        .expect("Expected PostableChannels in TypeMap");
+    if let Some((channel_id, link)) = scrape_message(&msg.content) {
+        if postable_channels.get(&channel_id).is_some() {
+            channel_id
+                .send_message(&ctx.http, |m| {
+                    m.content(link);
+
+                    m
+                })
+                .await
+                .unwrap();
+        }
+    }
+}
+
+/// Scrape the ChannelId and Link from a string input
+fn scrape_message<'a>(text: &'a str) -> Option<(ChannelId, &'a str)> {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"<#(\d+)>").unwrap();
+        static ref FINDER: LinkFinder = {
+            let mut finder = LinkFinder::new();
+            finder.kinds(&[LinkKind::Url]);
+
+            finder
+        };
+    }
+
+    let links: Vec<Link> = FINDER.links(text).collect();
+    if links.len() <= 0 {
+        return None;
+    }
+
+    let captures = match RE.captures(text) {
+        Some(captures) => captures,
+        None => return None,
+    };
+    let capture = match captures.get(1) {
+        Some(capture) => capture,
+        None => return None,
+    };
+    if let Ok(id) = capture.as_str().parse::<u64>() {
+        Some((ChannelId(id), links[0].as_str()))
+    } else {
+        None
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_grabs_channel_link() {
+        let result =
+            scrape_message("Check out this new game! https://kickstarter.com/ <#123456789>");
+
+        assert!(result.is_some());
+        if let Some((channel_id, link)) = result {
+            assert_eq!(123456789, *channel_id.as_u64());
+            assert_eq!("https://kickstarter.com/", link);
+        }
+    }
+
+    #[test]
+    fn it_returns_none_if_no_link() {
+        let result = scrape_message("Check out this new game! <#123456789>");
+        assert!(result.is_none());
+    }
+
+    #[test]
+    fn it_returns_none_if_no_channel() {
+        let result = scrape_message("Check out this new game! https://kickstarter.com/");
+        assert!(result.is_none());
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,9 @@
-use lazy_static::lazy_static;
-use linkify::{Link, LinkFinder, LinkKind};
-use regex::Regex;
+use daphuulbot::feed;
 use serenity::{
     async_trait,
     client::{Client, Context, EventHandler},
-    framework::{standard::macros::hook, StandardFramework},
-    model::{
-        gateway::Ready,
-        prelude::{ChannelId, Message},
-    },
-    prelude::TypeMapKey,
+    framework::{standard::macros::group, StandardFramework},
+    model::{gateway::Ready, prelude::ChannelId},
 };
 use std::{collections::HashSet, env};
 
@@ -23,62 +17,6 @@ impl EventHandler for Handler {
     }
 }
 
-struct PostableChannels;
-impl TypeMapKey for PostableChannels {
-    type Value = HashSet<ChannelId>;
-}
-
-/// Scrape the ChannelId and Link from a string input
-fn scrape_message<'a>(text: &'a str) -> Option<(ChannelId, &'a str)> {
-    lazy_static! {
-        static ref RE: Regex = Regex::new(r"<#(\d+)>").unwrap();
-        static ref FINDER: LinkFinder = {
-            let mut finder = LinkFinder::new();
-            finder.kinds(&[LinkKind::Url]);
-
-            finder
-        };
-    }
-
-    let links: Vec<Link> = FINDER.links(text).collect();
-    if links.len() <= 0 {
-        return None;
-    }
-
-    let captures = match RE.captures(text) {
-        Some(captures) => captures,
-        None => return None,
-    };
-    let capture = match captures.get(1) {
-        Some(capture) => capture,
-        None => return None,
-    };
-    if let Ok(id) = capture.as_str().parse::<u64>() {
-        Some((ChannelId(id), links[0].as_str()))
-    } else {
-        None
-    }
-}
-
-#[hook]
-async fn normal_message_hook(ctx: &Context, msg: &Message) {
-    let data = ctx.data.read().await;
-    let postable_channels = data
-        .get::<PostableChannels>()
-        .expect("Expected PostableChannels in TypeMap");
-    if let Some((channel_id, link)) = scrape_message(&msg.content) {
-        if postable_channels.get(&channel_id).is_some() {
-            channel_id
-                .send_message(&ctx.http, |m| {
-                    m.content(link);
-
-                    m
-                })
-                .await
-                .unwrap();
-        }
-    }
-}
 
 #[tokio::main]
 async fn main() {
@@ -92,7 +30,7 @@ async fn main() {
         .collect::<anyhow::Result<HashSet<ChannelId>>>()
         .expect("Channel IDs must be u64s");
 
-    let framework = StandardFramework::new().normal_message(normal_message_hook);
+    let framework = StandardFramework::new().normal_message(feed::normal_message_hook);
 
     let mut client = Client::builder(&token)
         .event_handler(Handler)
@@ -101,39 +39,10 @@ async fn main() {
         .expect("Error creating client.");
     {
         let mut data = client.data.write().await;
-        data.insert::<PostableChannels>(postable_channels);
+        data.insert::<feed::PostableChannels>(postable_channels);
     }
 
     if let Err(err) = client.start().await {
         eprintln!("Client error: {:?}", err);
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_grabs_channel_link() {
-        let result =
-            scrape_message("Check out this new game! https://kickstarter.com/ <#123456789>");
-
-        assert!(result.is_some());
-        if let Some((channel_id, link)) = result {
-            assert_eq!(123456789, *channel_id.as_u64());
-            assert_eq!("https://kickstarter.com/", link);
-        }
-    }
-
-    #[test]
-    fn it_returns_none_if_no_link() {
-        let result = scrape_message("Check out this new game! <#123456789>");
-        assert!(result.is_none());
-    }
-
-    #[test]
-    fn it_returns_none_if_no_channel() {
-        let result = scrape_message("Check out this new game! https://kickstarter.com/");
-        assert!(result.is_none());
     }
 }
